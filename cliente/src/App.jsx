023611f1958a48f6537c4a2851c8fd49276bbb5a
Pablo Andres Ramirez Lopez <pabloa1311@gmail.com>
2023-09-18 13:52:58 -0500

    Basic Layout created

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -1,12 +1,16 @@
-
+import Canvas from "./canvas"
+import Home from "./pages/Home"
+import Customizer from "./pages/Customizer"
 
 function App() {
  
 
   return (
-    <div>
-      <h1 className="head-text">React app</h1>
-    </div>
+    <main className="app transition-allease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
